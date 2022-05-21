2a181991a9cb85b7c9a78133f2ab5d489ac50568
Łukasz Gryl <ldgryl@gmail.com>
2022-05-21 14:48:56 +0200

    Refactor

--- TicTacToe/View/Views/GameView.swift
+++ TicTacToe/View/Views/GameView.swift
@@ -18,25 +18,50 @@ struct GameView: View {
                     HStack {
                         ForEach(0..<viewModel.game.boardSize, id: \.self) { column in
                             GameTileView(tile: viewModel.game.board[row][column])
-                            .onTapGesture {
-                                viewModel.makeMoveAt(row: row, column: column)
-                            }
+                                .onTapGesture {
+                                    viewModel.makeMoveAt(row: row, column: column)
+                                }
                         }
                     }
                 }
             }
             .padding()
             Spacer()
-            Button("Play again?") {
-                viewModel.resetGame()
+            HStack {
+                button(imageName: "chevron.backward", action: {
+                    print("Back")
+                })
+                .frame(maxWidth: 100)
+                Spacer()
+                button(imageName: "arrow.counterclockwise", action: {
+                    viewModel.resetGame()
+                })
+                .frame(maxWidth: 100)
             }
+            .padding(.horizontal)
         }
         .background(Color(white: 250 / 255))
+    }
+    
+    private func button(imageName: String, action: @escaping () -> Void) -> some View {
+        TileView {
+            Button {
+                action()
+            } label: {
+                Image(systemName: imageName)
+                    .font(.title)
+                    .foregroundStyle(LinearGradient(colors: [.blue, .purple], startPoint: .top, endPoint: .bottom))
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         GameView()
+            .previewDevice("iPhone 13 Pro Max")
+            .previewInterfaceOrientation(.portraitUpsideDown)
+        GameView()
+            .previewDevice("iPhone SE (3rd generation)")
     }
 }
